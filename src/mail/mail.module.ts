b6121bac18be6fdd0657b5fc2fb289797b8c612b
taofeeekishola <taofeekishola1791@gmail.com>
2025-11-17 20:54:21 +0100

    complete the configuration for mail module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,42 @@
-import { Module } from '@nestjs/common';
-import { MailService } from './providers/mail.service';
+import { Global, Module } from '@nestjs/common';
 
+import { ConfigService } from '@nestjs/config';
+import { MailService } from './providers/mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { join } from 'path';
+import {EjsAdapter} from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { strict } from 'assert';
+
+
+@Global() //allows it to be imported to any module in the application
 @Module({
-  providers: [MailService]
+  imports: [
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async(config: ConfigService)=>({
+        transport: {
+          host: config.get('appConfig.mailHost'),
+          secure: false,
+          port: 2525,
+          auth:{
+            user: config.get('appConfig.smtpUsername'),
+            pass: config.get('appConfig.smtpPassword'),
+          },
+        },
+        defaults:{
+          from: `My Blog <no-repl@nestjs-blog.com>`
+        },
+        template:{
+          dir: join(__dirname, 'templates'),
+          adapter: new EjsAdapter(),
+          options: {
+            strict: false,
+          },
+        },
+      }),
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}

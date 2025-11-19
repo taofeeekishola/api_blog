6a876fabfabd8a74882cb0f3f22b614a466a27fd
taofeeekishola <taofeekishola1791@gmail.com>
2025-11-19 23:37:47 +0100

    completed the createUserProvider unit test file for creaing a new user and checking if a user exists

--- src/users/providers/create-user.provider.spec.ts
+++ src/users/providers/create-user.provider.spec.ts
@@ -6,6 +6,7 @@ import { HashingProvider } from 'src/auth/provider/hashing.provider';
 import { DataSource, ObjectLiteral, Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 //creating a mock of the repository
 type MockRepository<T extends ObjectLiteral = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
@@ -33,7 +34,7 @@ describe('CreateUserProvider', () => {
                 {provide: getRepositoryToken(User), useValue: createMockRepository()},   
                 {
                     provide: MailService, 
-                    useValue: {sendUserWelcome: jest.fn(()=> Promise.resolve())}
+                    useValue: {senduserWelcome: jest.fn(()=> Promise.resolve())}
                 },   
                 {
                     provide: HashingProvider, 
@@ -46,10 +47,44 @@ describe('CreateUserProvider', () => {
         usersRepsitory = module.get(getRepositoryToken(User));
     });
 
-
+    //checks if the provider is defined
     it('should be defined', () => {
         expect(provider).toBeDefined();
     });
+
+    describe('createUser', () => {
+        describe('When the user does not exist in database', () =>{
+            it('should create a new user',  async () =>{
+                usersRepsitory.findOne?.mockReturnValue(null);
+                usersRepsitory.create?.mockReturnValue(user);
+                usersRepsitory.save?.mockReturnValueOnce(user);
+
+                //create a new user
+                const newUser = await provider.createUser(user);
+                expect(usersRepsitory.findOne).toHaveBeenCalledWith({
+                    where: {email: user.email},
+                });
+                expect(usersRepsitory.create).toHaveBeenCalledWith(user);
+                expect(usersRepsitory.save).toHaveBeenCalledWith(user);
+
+            })
+        });
+
+        describe('When the user does exist in database', () =>{
+            it('throw BadRequestException', async () =>{
+                usersRepsitory.findOne?.mockReturnValue(user.email);
+                usersRepsitory.create?.mockReturnValue(user);
+                usersRepsitory.save?.mockReturnValueOnce(user);
+
+                //try to create a new user
+                try{
+                    const newUser = await provider.createUser(user);
+                } catch(error){
+                    expect(error).toBeInstanceOf(BadRequestException);
+                }
+            })
+        });
+    })
 
 
 });

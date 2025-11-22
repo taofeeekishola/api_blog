cbb7372fee09fbd16cdae6b69781191795417dc2
taofeeekishola <taofeekishola1791@gmail.com>
2025-11-22 07:26:24 +0100

    created a new file to add middlware while creating the app

---
+++ src/app.create.ts
@@ -0,0 +1,43 @@
+import { INestApplication } from "@nestjs/common";
+
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; //swagger impotts
+
+
+export function appCreate(app:INestApplication): void{
+     /*
+    * Use Validation pipes globally 
+    */
+    app.useGlobalPipes(
+        new ValidationPipe({
+        whitelist: true, //prevents values not in the dto from entering our app
+        forbidNonWhitelisted: true, // throws an error if an extra value is passed from the user
+        transform: true, //this transforms the incoming request to an instance of the DTO class after validation
+        transformOptions:{
+            enableImplicitConversion: true, //this implicitly converts the validation types in the dto
+        }
+    })
+    );
+
+    /*
+    * swagger configuration
+    */
+    const config = new DocumentBuilder()
+    .setTitle("NestJs Masterclass - Blog app API")
+    .setDescription('Use the base API URL as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense(
+    'MIT License',
+    'https://aniwatchtv.to'
+    )
+    .addServer('http://localhost:3000')
+    .setVersion('1.0').build(); //creating a new document builder object
+
+    //Instantiate Document
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+
+    //enable cors
+    app.enableCors();
+}
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; //swagger impotts
-import { DataResponseInterceptor } from './common/interceptors/data-response/data-response.interceptor';
+
+import { appCreate } from './app.create';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  /*
-  * Use Validation pipes globally 
-  */
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, //prevents values not in the dto from entering our app
-      forbidNonWhitelisted: true, // throws an error if an extra value is passed from the user
-      transform: true, //this transforms the incoming request to an instance of the DTO class after validation
-      transformOptions:{
-        enableImplicitConversion: true, //this implicitly converts the validation types in the dto
-      }
-  })
-);
-
-/*
-* swagger configuration
-*/
-const config = new DocumentBuilder()
-.setTitle("NestJs Masterclass - Blog app API")
-.setDescription('Use the base API URL as http://localhost:3000')
-.setTermsOfService('http://localhost:3000/terms-of-service')
-.setLicense(
-  'MIT License',
-  'https://aniwatchtv.to'
-)
-.addServer('http://localhost:3000')
-.setVersion('1.0').build(); //creating a new document builder object
-
-//Instantiate Document
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
-
-
-//enable cors
-app.enableCors();
-await app.listen(process.env.PORT ?? 3000);
+  
+  //Add middleware
+  appCreate(app);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

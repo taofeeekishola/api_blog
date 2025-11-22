14de378c94da2556bfacea216dccfa402b0d2871
taofeeekishola <taofeekishola1791@gmail.com>
2025-11-22 07:59:16 +0100

    created usepost file for e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,8 +17,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    console.log(process.env.NODE_ENV)
-    console.log(process.env.S3_BUCKET)
     return request(app.getHttpServer())
       .get('/')
       .expect(404);
